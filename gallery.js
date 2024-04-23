54fa8f399e6aa9a8e8a4370f789955029caed939
gatya <gg@gg.hu>
2024-04-23 08:36:23 +0200

    tömb

---
+++ gallery.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+let tomb = ["alma", "körte", "kutya", "macska"];
+console.log(tomb);
+
+let div = document.createElement("div");
+div.innerHTML = "Hello";
+document.body.appendChild(div);
